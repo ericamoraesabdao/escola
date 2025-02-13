eae221afa132cd722caa9fa80ca712ca47bd189e
Erica <erica.abdao@zup.com.br>
2025-02-13 19:20:18 -0300

    Feat: Acrescenta a classe AlunosDTO.

---
+++ src/main/java/com/example/escola/dtos/AlunosDTO.java
@@ -0,0 +1,40 @@
+package com.example.escola.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class AlunosDTO {
+    private long id;
+    private String nome;
+    private int idade;
+
+    public AlunosDTO(long id, String nome, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
